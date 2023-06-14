d38af8cf9e10ae986b9b8ffdff2de0cca16e5049
LarryPinicola <thelarry1549@gmail.com>
2023-06-14 12:39:03 +0630

    paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import './App.scss'
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Paths from './routes/Paths';
 
 
 const App = () => {
   return (
     <>
     <Navbar/>
-    <Routes>
-        {/* <Route path='' element={}/> */}
-    </Routes> 
+    <Paths/>
     </>
    
   )
