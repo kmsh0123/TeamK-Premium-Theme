61c7ca404378de257776d7662d5f3c41cd855195
LarryPinicola <thelarry1549@gmail.com>
2023-06-14 12:36:17 +0630

    routes update

---
+++ src/routes/Paths.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Paths = () => {
+  return (
+    <div>
+        <Routes>
+            {/* <Route path='/' element={}/> */}
+        </Routes>
+    </div>
+  )
+}
+
+export default Paths
