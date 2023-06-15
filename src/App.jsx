26f33853607e052a840a4d7cdd607392b954cd4a
LarryPinicola <thelarry1549@gmail.com>
2023-06-15 19:36:38 +0630

    about page done all responsives

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import './App.scss'
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-
+import React from "react";
+import "./App.scss";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
 
 const App = () => {
+
+  const slides = {
+    
+  }
+
   return (
     <>
-    <Navbar/>
-    <Routes>
-        {/* <Route path='' element={}/> */}
-    </Routes> 
+      {/* <Navbar/> */}
+      <About />
+      <Routes>{/* <Route path='' element={}/> */}</Routes>
     </>
-   
-  )
-}
+  );
+};
 
-export default App
+export default App;
