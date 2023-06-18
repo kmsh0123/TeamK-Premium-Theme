bc73cdc8da41030047a8c5426482d8ad35459653
unknown <komauk1998@gmail.com>
2023-06-18 10:43:00 +0630

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.scss'
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import SecNavbar from './components/SecNavbar';
 
 
 const App = () => {
   return (
     <>
     <Navbar/>
-    
+    {/* <SecNavbar/> */}
     </>
    
   )
