24d66f2dafd06c49ba2cf16b0e4a9a8ef9eca719
DELL <nanshwethi59@gmail.com>
2023-06-18 16:40:00 +0630

    add home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.scss'
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Paths from './routes/Paths';
+import HomePage from './components/HomePage';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <Paths/>
+    {/* <Navbar/> */}
+    <HomePage/>
     </>
    
   )
